1b5b628bb87ad6a49f5f6caa69c303444a218c64
nana0739ice <nana0739ice@gmail.com>
2019-04-15 11:55:29 +0800

    cipher

---
+++ cipher.py
@@ -0,0 +1,53 @@
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+MAX_KEY_SIZE = len(SYMBOLS)
+
+def getMode():
+    while True:
+        print('Do you wish to encrypt or decrypt or brute-force a message?')
+        print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
+        mode = input().lower()
+        if mode in ['encrypt', 'e', 'decrypt', 'd', 'brute', 'b']:
+            return mode            
+
+def getMessage():
+    print("Enter your message:")
+    return input()
+
+def getKey():
+    key = 0
+    while True:
+        print('Enter the key number(1-%s)' % (MAX_KEY_SIZE))
+        key = input()
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'd':
+        key = -key
+    translated = ''
+
+    for symbol in message:
+        symbolIndex = SYMBOLS.find(symbol)
+        if symbolIndex == -1:
+            translated += symbol
+        else:
+            symbolIndex += key
+
+            if symbolIndex >= len(SYMBOLS):
+                symbolIndex -= len(SYMBOLS)
+            elif symbolIndex < 0:
+                symbolIndex += len(SYMBOLS)
+
+            translated += SYMBOLS[symbolIndex]
+    return translated
+
+mode = getMode()
+message = getMessage()
+if mode[0] != 'b':
+    key = getKey()
+print('Your translated text is:')
+if mode[0] != 'b':
+    print(getTranslatedMessage(mode, message, key))
+else:
+    for key in range(1, MAX_KEY_SIZE+1):
+        print(key, getTranslatedMessage(mode, message, key))
